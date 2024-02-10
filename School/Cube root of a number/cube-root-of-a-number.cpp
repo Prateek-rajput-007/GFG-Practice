62fc565101bb2873deb4249ad6a2483bd86f2bdd
Prateek <109753518+Prateek-rajput-007@users.noreply.github.com>
2024-02-10 23:12:16 +0530

    Added Solution - GfG to GitHub

---
+++ School/Cube root of a number/cube-root-of-a-number.cpp
@@ -0,0 +1,29 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int cubeRoot(int N) {
+        // code here
+                return pow(N,0.33333333);
+
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+
+        cin>>N;
+
+        Solution ob;
+        cout << ob.cubeRoot(N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
