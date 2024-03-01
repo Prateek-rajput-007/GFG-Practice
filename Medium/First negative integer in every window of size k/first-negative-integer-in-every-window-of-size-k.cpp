03a332d92760ed4c57b5bc7127f7a2ccb4aa822b
Prateek <109753518+Prateek-rajput-007@users.noreply.github.com>
2024-03-01 14:31:42 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/First negative integer in every window of size k/first-negative-integer-in-every-window-of-size-k.cpp
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<long long> printFirstNegativeInteger(long long int arr[],
+                                             long long int n, long long int k);
+
+// Driver program to test above functions
+int main() {
+    long long int t, i;
+    cin >> t;
+    while (t--) {
+        long long int n;
+        cin >> n;
+        long long int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        long long int k;
+        cin >> k;
+
+        vector<long long> ans = printFirstNegativeInteger(arr, n, k);
+        for (auto it : ans) cout << it << " ";
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+
+
+
+#include<vector>
+#include<deque>
+vector<long long> printFirstNegativeInteger(long long int A[],
+                                             long long int N, long long int K) {
+                                                 
+         deque<long long> q;
+         vector<long long>ans;
+    //process of first window
+    for(int i=0;i<K;i++){
+        if(A[i]<0){
+            q.push_back(i);
+        }
+    }
+    //remaining window ka process
+    for(int i=K;i<N;i++){
+        //answer insert kro
+         if(q.empty()){
+            ans.push_back(0);
+        }
+        else{
+            ans.push_back(A[q.front()]);
+        }
+        //out of window ko remove kiya
+        while((!q.empty())&&((i-q.front())>=K)){
+            q.pop_front();
+        }
+        //check current element for insertion
+         if(A[i]<0){
+            q.push_back(i);
+        }
+    }
+     if(q.empty()){
+            ans.push_back(0);
+        }
+        else{
+            ans.push_back(A[q.front()]);
+        }
+        return ans;
+ }
